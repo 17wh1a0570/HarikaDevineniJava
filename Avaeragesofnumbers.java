70e6923efe5c049238c7e174c052c16bcd54a9dc
Harika Devineni <60818916+17wh1a0570@users.noreply.github.com>
2020-08-02 16:24:04 +0530

    Update Avaeragesofnumbers.java

--- Avaeragesofnumbers.java
+++ Avaeragesofnumbers.java
@@ -1,23 +1,23 @@
 package programs;
 
 import java.util.*;
-public class Avaeragesofnumbers {
+
+public class Main {
 	public static void main(String[] args) {
-				Scanner s = new Scanner(System.in);
-				System.out.println("Enter num");
-				int num = s.nextInt();
-				ArrayList<Integer> A = new ArrayList<>();
-				System.out.println("Enter elements in list");
-				s.nextLine();
-				for(int i = 0;i < num;i++){ 
-					A.add(s.nextInt());
-				}
-				int sum = A.stream().mapToInt(i -> i.intValue()).sum();
-				double avg=0;
-				int b=A.size();
-				if(b > 0) {
-					 avg = sum / A.size();
-				}
-				System.out.println("Average of the numbers "+avg);
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		ArrayList<Integer> array = new ArrayList<>();
+		System.out.print("Enter array elements: ");
+		scan.nextLine();
+		for(int i = 0; i < number; ++i) {
+			array.add(scan.nextInt());
+		}
+		int sumOfElements = array.stream().mapToInt(i -> i.intValue()).sum();
+		double avg = 0;
+		int b = array.size();
+		if(b > 0) {
+			avg = sumOfElements / array.size();
+		}
+		System.out.println("Average of the numbers " + avg);
 	}
 }
